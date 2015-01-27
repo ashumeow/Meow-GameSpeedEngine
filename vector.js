637c86019fa646eea9ecb5c0489337fa09922176
Aswini S <ashumeow@live.com>
2015-01-27 17:21:33 +0530

    Create vector.js

---
+++ vector.js
@@ -0,0 +1,18 @@
+var vector = function(X2d) {
+	'use strict';
+	var meow = this;
+	X2d.vector = function(x, y) {
+		meow.x = x || 0;
+		meow.y = y || 0;
+	};
+	xx.extend(vector, {
+		zero: new X2d.vector()
+	});
+	xx.extend(vector.prototype, main.events, {
+
+		//
+		// Still more to code
+		//
+
+	});
+};
